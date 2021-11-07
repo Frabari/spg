8e67438fa2603c6394fd60e12e783814b2157b26
Federico Pregliasco <s284552@studenti.polito.it>
2021-11-07 16:57:15 +0100

    feat(backend): add all feature modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './features/users/users.module';
+import { ProductsModule } from './features/products/products.module';
+import { CategoriesModule } from './features/categories/categories.module';
+import { StockModule } from './features/stock/stock.module';
+import { OrdersModule } from './features/orders/orders.module';
+import { TransactionsModule } from './features/transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    StockModule,
+    OrdersModule,
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
