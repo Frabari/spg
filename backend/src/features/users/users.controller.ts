9d2ada1a11ffe6c249fd54c1f836254b3ee8fb94
Federico Pregliasco <s284552@studenti.polito.it>
2021-11-08 11:40:05 +0100

    feat(backend): implement user read endpoints

--- backend/src/features/users/users.controller.ts
+++ backend/src/features/users/users.controller.ts
@@ -1,4 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
+@Crud({
+  model: {
+    type: User,
+  },
+  routes: {
+    only: ['getOneBase', 'getManyBase'],
+  },
+})
+@ApiTags(User.name)
 @Controller('users')
-export class UsersController {}
+export class UsersController implements CrudController<User> {
+  constructor(public service: UsersService) {}
+}
--- backend/src/features/users/users.service.ts
+++ backend/src/features/users/users.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {
+export class UsersService extends TypeOrmCrudService<User> {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
-  ) {}
-
-  findOne(email: string) {
-    return this.usersRepository.findOne({ email });
+  ) {
+    super(usersRepository);
   }
 
   async validateUser(email: string, pass: string) {
-    const user = await this.findOne(email);
+    const user = await this.findOne({ email });
     if (user?.password === pass) {
       const { password, ...result } = user;
       return result;
