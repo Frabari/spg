73eb64a9e5504ee0cf22334162c374a6239fd154
Federico Pregliasco <s284552@studenti.polito.it>
2021-11-08 11:21:28 +0100

    feat(backend): setup Passport environment

--- backend/src/features/users/users.service.ts
+++ backend/src/features/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  findOne(email: string) {
+    return this.usersRepository.findOne({ email });
+  }
+
+  async validateUser(email: string, pass: string) {
+    const user = await this.findOne(email);
+    if (user?.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
