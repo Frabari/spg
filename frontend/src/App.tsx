eefde37ae403e544fabda6f7474d43fecab5a7e1
Simona <simonarobustella@gmail.com>
2021-11-09 12:21:14 +0100

    feat: implemented shell component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage';
+import { ThemeProvider} from '@mui/material/styles';
+import { themeOptions } from './pages/CustomTheme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeOptions}>
+      <Router>
+        <div className="App">
+
+          <Route exact path="/">    
+            <Redirect to="/home" />
+          </Route>
+
+          <Route exact path="/home">
+            <Homepage />
+          </Route>
+
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
