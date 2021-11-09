0a6236748e1ac2e0abdd3fc0a2685a505b93107f
Umberto Pepato <umbo.pepato@gmail.com>
2021-11-09 12:57:47 +0100

    test(backend): setup unit test imports

--- backend/src/features/users/users.controller.spec.ts
+++ backend/src/features/users/users.controller.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users.module';
+import { ProductsModule } from '../products/products.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { StockModule } from '../stock/stock.module';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { OrdersModule } from '../orders/orders.module';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          autoLoadEntities: true,
+          dropSchema: true,
+          synchronize: true,
+        }),
+        UsersModule,
+        ProductsModule,
+        CategoriesModule,
+        StockModule,
+        TransactionsModule,
+        OrdersModule,
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- backend/src/features/users/users.service.spec.ts
+++ backend/src/features/users/users.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users.module';
+import { ProductsModule } from '../products/products.module';
+import { CategoriesModule } from '../categories/categories.module';
+import { StockModule } from '../stock/stock.module';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { OrdersModule } from '../orders/orders.module';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          autoLoadEntities: true,
+          dropSchema: true,
+          synchronize: true,
+        }),
+        UsersModule,
+        ProductsModule,
+        CategoriesModule,
+        StockModule,
+        TransactionsModule,
+        OrdersModule,
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
