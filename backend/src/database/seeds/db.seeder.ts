e34af3d94287d49d3540a4ab18eabac4c60111cc
Francesco Rabezzano <francescorabezzano@gmail.com>
2021-11-10 17:44:00 +0100

    fix(backend): change assignment role to users

--- backend/src/database/seeds/db.seeder.ts
+++ backend/src/database/seeds/db.seeder.ts
@@ -11,41 +11,61 @@ let firstN = -1;
 function checkFarmer(role: any, n: number, products: Product[]) {
   let prods = [];
 
-  if(firstN === -1)
-    firstN = n;
-  if( role === Role.FARMER && firstN === n) {
+  if (firstN === -1) firstN = n;
+  if (role === Role.FARMER && firstN === n) {
     prods = products.slice(0, 25);
-  } else if( role === Role.FARMER && firstN !== n)
+  } else if (role === Role.FARMER && firstN !== n)
     prods = products.slice(25, 50);
 
   return prods;
 }
 
-
 export default class DbSeeder implements Seeder {
   public async run(factory: Factory, connection: Connection) {
     const entityManager = connection.createEntityManager();
-    const products = await entityManager.save(await factory(Product)().createMany(50));
+    const products = await entityManager.save(
+      await factory(Product)().createMany(50),
+    );
 
-    Object.entries(Role).forEach(([roleName, role]) => {
+    Object.values(Role).forEach(role => {
       [1, 2].forEach(n => {
         entityManager.save(User, {
           role: role,
-          email: `${roleName.toLowerCase()}${n}@example.com`,
+          email: `${role}${n}@example.com`,
           name: faker.name.firstName(),
           surname: faker.name.lastName(),
           password: passwordTest,
-          products: checkFarmer(role, n, products)
+          products: checkFarmer(role, n, products),
         });
-      })
-    })
+      });
+    });
 
     await entityManager.save(Category, [
-      { id: 1, name: 'Vegetables', slug: 'vegetables', products: products.slice(0, 10)},
-      { id: 2, name: 'Fruits', slug: 'fruits', products: products.slice(10, 20)},
-      { id: 3, name: 'Dairy Products', slug: 'dairy-products', products: products.slice(20, 30)},
-      { id: 4, name: 'Breeding', slug: 'breeding', products: products.slice(30, 40)},
-      { id: 5, name: 'Meat', slug: 'meat', products: products.slice(40, 50)}
+      {
+        id: 1,
+        name: 'Vegetables',
+        slug: 'vegetables',
+        products: products.slice(0, 10),
+      },
+      {
+        id: 2,
+        name: 'Fruits',
+        slug: 'fruits',
+        products: products.slice(10, 20),
+      },
+      {
+        id: 3,
+        name: 'Dairy Products',
+        slug: 'dairy-products',
+        products: products.slice(20, 30),
+      },
+      {
+        id: 4,
+        name: 'Breeding',
+        slug: 'breeding',
+        products: products.slice(30, 40),
+      },
+      { id: 5, name: 'Meat', slug: 'meat', products: products.slice(40, 50) },
     ]);
   }
 }
