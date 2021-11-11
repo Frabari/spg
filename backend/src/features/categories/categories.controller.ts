8cd5aeda618f22bec3761b5a6c1936fe644b92bf
Umberto Pepato <umbo.pepato@gmail.com>
2021-11-11 22:12:21 +0100

    feat(backend): add get categories endpoint

--- backend/src/features/categories/categories.controller.ts
+++ backend/src/features/categories/categories.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { Crud } from '../../core/decorators/crud.decorator';
+import { Category } from './entities/category.entity';
+import { CategoriesService } from './categories.service';
 
+@Crud(Category, {
+  routes: {
+    only: ['getManyBase'],
+  },
+})
+@ApiTags(Category.name)
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(public readonly service: CategoriesService) {}
+}
