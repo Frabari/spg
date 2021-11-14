0abee6b1fb885b9e9cf543a6e84572a9dbc5f79c
Enrico <er.enricodoro@gmail.com>
2021-11-14 13:44:11 +0100

    fix: insert first entry on selecting product

--- frontend/src/components/ProductsGrid.tsx
+++ frontend/src/components/ProductsGrid.tsx
@@ -1,91 +1,94 @@
 import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Grid,
-  IconButton,
-  Typography,
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid,
+    IconButton,
+    Typography,
 } from '@mui/material';
-import { useProducts } from '../hooks/useProducts';
+import {useProducts} from '../hooks/useProducts';
 import AddIcon from '@mui/icons-material/Add';
-import { useState } from 'react';
+import {useState} from 'react';
 import ProductInfo from '../pages/ProductInfo';
-import { Product } from '../api/basil-api';
+import {Product} from '../api/basil-api';
 
 function ProductCard(props: any) {
-  const [open, setOpen] = useState(false);
-  const [color, setColor] = useState('#ffff00');
+    const [open, setOpen] = useState(false);
 
-  const handleProduct = () => {
-    if (props.onSelect) {
-      props.onSelect(props.product);
-    } else {
-      setOpen(true);
+    const handleInfo = () => {
+        if (!props.onSelect) {
+            setOpen(true)
+        }
     }
-  };
 
-  return (
-    <Grid item lg={3} md={4} sm={6} xs={12}>
-      <ProductInfo open={open} setOpen={setOpen} {...props} />
-      <Card sx={{ height: '400' }}>
-        <CardMedia
-          component="img"
-          height="200"
-          width="200"
-          image={props.image}
-          onClick={handleProduct}
-        />
-        <CardContent onClick={handleProduct}>
-          <Typography gutterBottom variant="h5" component="div" align="center">
-            {props.name}
-          </Typography>
-          <Typography variant="body1" color="text.secondary" align="center">
-            {props.product.available} kg available
-          </Typography>
-          <Typography variant="body2" color="text.secondary" align="center">
-            € {props.price}/kg
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Box marginLeft="auto" padding="0.5rem">
-            <IconButton>
-              <AddIcon />
-            </IconButton>
-          </Box>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
+    const handleSelect = () => {
+        if (props.onSelect) {
+            props.onSelect(props.product)
+        }
+    }
+
+    return (
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+            <ProductInfo open={open} setOpen={setOpen} {...props} />
+            <Card sx={{height: '400'}}>
+                <CardMedia
+                    component="img"
+                    height="200"
+                    width="200"
+                    image={props.image}
+                    onClick={handleInfo}
+                />
+                <CardContent onClick={handleInfo}>
+                    <Typography gutterBottom variant="h5" component="div" align="center">
+                        {props.name}
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary" align="center">
+                        {props.product.available} kg available
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" align="center">
+                        € {props.price}/kg
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Box marginLeft="auto" padding="0.5rem">
+                        <IconButton onClick={handleSelect}>
+                            <AddIcon/>
+                        </IconButton>
+                    </Box>
+                </CardActions>
+            </Card>
+        </Grid>
+    )
 }
 
 export default function ProductsGrid(props: {
-  onSelect: (product: Product) => void;
+    onSelect: (product: Product) => void;
 }) {
-  const { products } = useProducts();
+    const {products} = useProducts();
 
-  return (
-    <Grid
-      container
-      direction="row"
-      spacing="2rem"
-      padding="1rem"
-      alignItems="center"
-      justifyItems="center"
-      width="auto"
-    >
-      {products?.map(p => (
-        <ProductCard
-          key={p.id}
-          name={p.name.split(' ')[2]}
-          image={p.image}
-          price={p.price}
-          description={p.description}
-          product={p}
-          onSelect={props.onSelect}
-        />
-      ))}
-    </Grid>
-  );
+    return (
+        <Grid
+            container
+            direction="row"
+            spacing="2rem"
+            padding="1rem"
+            alignItems="center"
+            justifyItems="center"
+            width="auto"
+        >
+            {products?.map(p => (p.available > 0 ?
+                    <ProductCard
+                        key={p.id}
+                        name={p.name.split(' ')[2]}
+                        image={p.image}
+                        price={p.price}
+                        description={p.description}
+                        product={p}
+                        onSelect={props.onSelect}
+                    /> : <></>
+            ))}
+        </Grid>
+    )
 }
