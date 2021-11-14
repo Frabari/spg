b1d13350522b675ca6f60a8f57692ce5aecd4aaf
Umberto Pepato <umbo.pepato@gmail.com>
2021-11-14 17:56:28 +0100

    feat: implement transaction user balance update logic

--- backend/src/features/transactions/transactions.service.ts
+++ backend/src/features/transactions/transactions.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Repository } from 'typeorm';
+import { UsersService } from '../users/users.service';
 import { CreateTransactionDto } from './dtos/create-transaction.dto';
 import { Transaction } from './entities/transaction.entity';
 
@@ -10,11 +15,26 @@ export class TransactionsService extends TypeOrmCrudService<Transaction> {
   constructor(
     @InjectRepository(Transaction)
     private readonly transactionsRepository: Repository<Transaction>,
+    private readonly usersService: UsersService,
   ) {
     super(transactionsRepository);
   }
 
   async checkTransaction(dto: CreateTransactionDto) {
+    const user = await this.usersService.findOne(dto.user.id);
+    if (!user) {
+      throw new NotFoundException(
+        'Transaction.UserNotFound',
+        'Transaction user not found',
+      );
+    }
+    if (user.balance + dto.amount < 0) {
+      throw new BadRequestException(
+        'Transaction.InsufficientBalance',
+        `The user's balance cannot satisfy this transaction`,
+      );
+    }
+    await this.usersService.updateBalance(user, dto.amount);
     return dto;
   }
 }
