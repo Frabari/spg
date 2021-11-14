f3648f471320cf62dd4d16bcc8c24c6a66147446
Federico Pregliasco <s284552@studenti.polito.it>
2021-11-14 17:22:45 +0100

    fix(backend): correct orders and add transactions

--- backend/src/features/transactions/transactions.service.ts
+++ backend/src/features/transactions/transactions.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Repository } from 'typeorm';
+import { CreateTransactionDto } from './dtos/create-transaction.dto';
+import { Transaction } from './entities/transaction.entity';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService extends TypeOrmCrudService<Transaction> {
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionsRepository: Repository<Transaction>,
+  ) {
+    super(transactionsRepository);
+  }
+
+  async checkTransaction(dto: CreateTransactionDto) {
+    return dto;
+  }
+}
