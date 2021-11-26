5941c2b82a43d01021d39929907b83d9a29f23ea
Frabari <francyjuve95@gmail.com>
2021-11-26 18:52:47 +0100

    fix: add some fixes

--- backend/src/features/orders/orders.service.spec.ts
+++ backend/src/features/orders/orders.service.spec.ts
@@ -1,21 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { OrdersService } from './orders.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from '../users/users.module';
-import { ProductsModule } from '../products/products.module';
-import { CategoriesModule } from '../categories/categories.module';
-import { TransactionsModule } from '../transactions/transactions.module';
-import { OrdersModule } from './orders.module';
-import { EntityManager } from 'typeorm';
-import { Order, OrderStatus } from './entities/order.entity';
-import { User } from '../users/entities/user.entity';
 import { hash } from 'bcrypt';
-import { Product } from '../products/entities/product.entity';
 import { DateTime } from 'luxon';
-import { OrderEntry } from './entities/order-entry.entity';
-import { CreateOrderDto } from './dtos/create-order.dto';
+import { EntityManager } from 'typeorm';
 import { BadRequestException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from '../categories/categories.module';
+import { Product } from '../products/entities/product.entity';
+import { ProductsModule } from '../products/products.module';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { User } from '../users/entities/user.entity';
+import { Role } from '../users/roles.enum';
+import { UsersModule } from '../users/users.module';
+import { CreateOrderDto } from './dtos/create-order.dto';
 import { UpdateOrderDto } from './dtos/update-order.dto';
+import { OrderEntry } from './entities/order-entry.entity';
+import { Order, OrderStatus } from './entities/order.entity';
+import { OrdersModule } from './orders.module';
+import { OrdersService } from './orders.service';
 
 describe('OrdersService', () => {
   let service: OrdersService;
@@ -59,7 +60,7 @@ describe('OrdersService', () => {
         price: 10,
         available: 10,
       });
-      expect(
+      return expect(
         service.checkOrder({
           user: { id: user.id } as User,
           entries: [{ product, quantity: 5 }] as OrderEntry[],
@@ -89,7 +90,7 @@ describe('OrdersService', () => {
         price: 10,
         available: 10,
       });
-      expect(
+      return expect(
         service.checkOrder({
           user: { id: user.id } as User,
           entries: [{ product, quantity: 20 }] as OrderEntry[],
@@ -99,7 +100,7 @@ describe('OrdersService', () => {
   });
 
   describe('checkOrderUpdate', () => {
-    it('should cheange the status of the order with a following one', async () => {
+    it('should change the status of the order with a following one', async () => {
       const email = 'test@example.com';
       const password = 'testpwd';
       const entityManager = module.get(EntityManager);
@@ -113,9 +114,13 @@ describe('OrdersService', () => {
         user: { id: user.id },
       });
       expect(
-        await service.checkOrderUpdate(order.id, {
-          status: OrderStatus.PAID,
-        } as UpdateOrderDto),
+        await service.checkOrderUpdate(
+          order.id,
+          {
+            status: OrderStatus.PAID,
+          } as UpdateOrderDto,
+          { role: Role.MANAGER } as User,
+        ),
       ).toMatchObject({ status: OrderStatus.PAID });
     });
 
@@ -134,11 +139,64 @@ describe('OrdersService', () => {
         user: { id: user.id },
         status: OrderStatus.PAID,
       });
-      expect(
-        service.checkOrderUpdate(order.id, {
-          status: OrderStatus.DRAFT,
-        } as UpdateOrderDto),
+      return expect(
+        service.checkOrderUpdate(
+          order.id,
+          {
+            status: OrderStatus.DRAFT,
+          } as UpdateOrderDto,
+          user,
+        ),
       ).rejects.toThrowError(BadRequestException);
+    });
+
+    it('should fail if the order status is locked', async () => {
+      const email = 'test@example.com';
+      const password = 'testpwd';
+      const entityManager = module.get(EntityManager);
+      const user = await entityManager.save(User, {
+        email,
+        password: await hash(password, 10),
+        name: 'John',
+        surname: 'Doe',
+        role: Role.CUSTOMER,
+      });
+      const product = await entityManager.save(Product, {
+        name: 'onions',
+        description: 'very good onions',
+        price: 10,
+        available: 10,
+      });
+      let order = await entityManager.save(Order, {
+        status: OrderStatus.DRAFT,
+        user: { id: user.id },
+        entries: [
+          {
+            product: {
+              id: product.id,
+            },
+            quantity: 5,
+          },
+        ],
+      });
+      await service.checkOrderUpdate(
+        order.id,
+        {
+          status: OrderStatus.LOCKED,
+          entries: [
+            {
+              ...order.entries[0],
+              quantity: 10,
+            },
+          ],
+        } as UpdateOrderDto,
+        user,
+      );
+
+      order = await entityManager.findOne(Order, order.id, {
+        relations: ['entries', 'entries.product'],
+      });
+      expect(order.entries[0].quantity).toEqual(5);
     });
   });
 
