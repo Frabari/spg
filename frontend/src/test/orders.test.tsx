a7863eb9594047251a0a4c33b706a8f9a2561608
Enrico <er.enricodoro@gmail.com>
2021-11-26 11:39:13 +0100

    test(frontend): add tests for categories, orders, products and users

--- frontend/src/test/orders.test.tsx
+++ frontend/src/test/orders.test.tsx
@@ -1,18 +1,35 @@
+import { waitFor } from '@testing-library/react';
 import { act, renderHook } from '@testing-library/react-hooks';
 import { Order } from '../api/BasilApi';
 import { PendingStateContext } from '../contexts/pending';
 import { useOrder } from '../hooks/useOrder';
 
+// @ts-ignore
+const wrapper = ({ children }) => (
+  <PendingStateContext.Provider
+    value={{ pending: true, setPending: (value: boolean) => true }}
+  >
+    {children}
+  </PendingStateContext.Provider>
+);
+
 jest.mock('../api/BasilApi', () => {
   const originalModule = jest.requireActual('../api/BasilApi');
-  const order: Partial<Order> = {
+  let order: Partial<Order> = {
     id: 30,
-    deliveryLocation: 'Turin',
   };
   return {
     __esModule: true, // Use it when dealing with esModules
     ...originalModule,
-    createOrder: (_order: Partial<Order>) => Promise.resolve(order),
+    createOrder: (_order: Partial<Order>) => {
+      order = _order;
+      return Promise.resolve(order);
+    },
+    updateOrder: (_id: number, _order: Partial<Order>) => {
+      order = _order;
+      return Promise.resolve(order);
+    },
+    getOrder: (_id: number) => Promise.resolve(order),
   };
 });
 
@@ -20,19 +37,31 @@ test('create order', async () => {
   const order: Partial<Order> = {
     deliveryLocation: 'Turin',
   };
-  // @ts-ignore
-  const wrapper = ({ children }) => (
-    <PendingStateContext.Provider
-      value={{ pending: true, setPending: (value: boolean) => true }}
-    >
-      {children}
-    </PendingStateContext.Provider>
-  );
 
   const { result } = renderHook(() => useOrder(), { wrapper });
   await act(async () =>
     expect(
       ((await result.current.upsertOrder(order)) as Order).deliveryLocation,
     ).toEqual('Turin'),
+  );
+});
+
+test('update order', async () => {
+  const order: Partial<Order> = {
+    id: 30,
+    deliveryLocation: 'Milan',
+  };
+  const { result } = renderHook(() => useOrder(30), { wrapper });
+  await act(async () =>
+    expect(
+      ((await result.current.upsertOrder(order)) as Order).deliveryLocation,
+    ).toEqual('Milan'),
+  );
+});
+
+test('get order', async () => {
+  const { result } = renderHook(() => useOrder(30), { wrapper });
+  await waitFor(() =>
+    expect(result.current.order.deliveryLocation).toEqual('Milan'),
   );
 });
