b84d27e52fe49e66c0827a6e0d4c5cd7b47fc2c6
Umberto Pepato <umbo.pepato@gmail.com>
2021-11-26 12:36:06 +0100

    feat: create @Computed decorator and interceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 import { version } from '../package.json';
 import { AppModule } from './app.module';
-import { User } from './features/users/entities/user.entity';
+import { validation } from './constants';
+import { ComputedInterceptor } from './core/interceptors/computed.interceptor';
 import { Category } from './features/categories/entities/category.entity';
 import { Order } from './features/orders/entities/order.entity';
 import { Product } from './features/products/entities/product.entity';
 import { Transaction } from './features/transactions/entities/transaction.entity';
-import { validation } from './constants';
+import { User } from './features/users/entities/user.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe(validation));
+  app.useGlobalInterceptors(new ComputedInterceptor());
   const config = new DocumentBuilder()
     .setTitle('Basil API')
     .setDescription('Solidarity purchase groups')
@@ -28,4 +30,5 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
   await app.listen(3001);
 }
+
 bootstrap();
