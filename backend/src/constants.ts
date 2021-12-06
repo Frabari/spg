53f94f7cdce736f92c05f47c90b0f9cd00e517de
Umberto Pepato <umbo.pepato@gmail.com>
2021-12-06 16:26:57 +0100

    refactor: use custom class-validator exceptionFactory

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,2 +1,32 @@
-export const validation = { whitelist: true, transform: true };
-export const CLOCK = 'CLOCK';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipeOptions,
+} from '@nestjs/common';
+
+export interface ErrorsObject {
+  [key: string]: string | ErrorsObject;
+}
+
+const errorsToObject = (errors: ValidationError[]) => {
+  const res: ErrorsObject = {};
+  errors.forEach(e => {
+    if (e.children?.length) {
+      res[e.property] = errorsToObject(e.children);
+    } else {
+      res[e.property] = [res[e.property], Object.values(e.constraints)]
+        .filter(i => i != null)
+        .join(', ');
+    }
+  });
+  return res;
+};
+
+export const validation: ValidationPipeOptions = {
+  whitelist: true,
+  transform: true,
+  exceptionFactory: errors =>
+    new BadRequestException({
+      constraints: errorsToObject(errors),
+    }),
+};
