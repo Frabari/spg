8c17a94f07528e457158298bdf90072678e21bb8
Umberto Pepato <umbo.pepato@gmail.com>
2021-12-10 17:28:59 +0100

    feat: add email notification channel

--- backend/src/features/notifications/notifications.service.ts
+++ backend/src/features/notifications/notifications.service.ts
@@ -1,9 +1,14 @@
-import { FindManyOptions, Repository } from 'typeorm';
+import { FindConditions, Repository } from 'typeorm';
+import { MailerService } from '@nestjs-modules/mailer';
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User, UserId } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
-import { Notification } from './entities/notification.entity';
+import {
+  Notification,
+  NotificationPriority,
+  NotificationType,
+} from './entities/notification.entity';
 import { NotificationsGateway } from './notifications.gateway';
 
 @Injectable()
@@ -16,6 +21,7 @@ export class NotificationsService {
     private readonly usersService: UsersService,
     @Inject(forwardRef(() => NotificationsGateway))
     private readonly notificationsGateway: NotificationsGateway,
+    private readonly mailerService: MailerService,
   ) {}
 
   activateUser(id: UserId) {
@@ -27,18 +33,41 @@ export class NotificationsService {
   }
 
   async sendNotification(
-    notification: Notification,
-    to: FindManyOptions<User>,
+    notification: Partial<Notification>,
+    to: FindConditions<User>,
   ) {
     const users = await this.usersService.find(to);
+    if (!users?.length) {
+      return;
+    }
     let loggedUsers: User[] = [];
     if (users?.length) {
       loggedUsers = users.filter(u => u.id in this.activeUserIds);
+    }
+    if (!('type' in notification)) {
+      notification.type = NotificationType.INFO;
+    }
+    if (!('persistent' in notification)) {
+      notification.persistent = true;
+    }
+    if (!('priority' in notification)) {
+      notification.priority = NotificationPriority.INFO;
     }
     this.notificationsGateway.server
       .in(loggedUsers.map(u => u.id.toString()))
       .emit('notification', notification);
     notification.deliveredTo = users;
-    await this.notificationsRepository.save(notification);
+    if (notification.priority === NotificationPriority.CRITICAL) {
+      users?.forEach(u => {
+        this.mailerService.sendMail({
+          to: u.email,
+          subject: `New ${notification.type} notification from Basil`,
+          text: notification.title + '\n\n' + notification.message,
+        });
+      });
+    }
+    if (notification.persistent) {
+      await this.notificationsRepository.save(notification);
+    }
   }
 }
