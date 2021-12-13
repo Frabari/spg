0c856b18a26e352083e0011de39efd6f51fe8369
Simona <simonarobustella@gmail.com>
2021-12-13 11:46:10 +0100

    fix(frontend): fix add basket time condition

--- frontend/src/components/ProductsGrid.tsx
+++ frontend/src/components/ProductsGrid.tsx
@@ -48,7 +48,7 @@ function ProductCard({
 
   const handleSelect = (product: Product) => {
     if (
-      DateTime.now() >= moment().day('saturday').hour(9) &&
+      DateTime.now() >= moment().day('saturday').hour(9) ||
       DateTime.now() <= moment().day('sunday').hour(23).minutes(0)
     ) {
       if (onSelect) {
